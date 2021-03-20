9b69ce830e140cde0654779da483923c9f5d6386
Gregor Adams <gregor.adams@evernest.com>
2021-03-20 19:19:57 +0100

    test: adjust test cases

--- cypress/integration/Dropdown/index.js
+++ cypress/integration/Dropdown/index.js
@@ -6,20 +6,20 @@ Given("I am on the create screen", function () {
 	cy.get(dataTestId("inner-frame")).should("be.visible");
 });
 
-When(`I click a link in the header menu`, label => {
+When(`I click a link in the header menu`, function () {
 	cy.window().reload();
 	cy.get(dataTestId("header-menu-link")).click();
 });
 
-Then(`I see a dropdown`, locale => {
+Then(`I see a dropdown`, function () {
 	cy.get(dataTestId("dropdown")).should("be.visible");
 });
 
-When(`I click outside the dropdown`, label => {
+When(`I click outside the dropdown`, function () {
 	cy.window().reload();
-	cy.get(dataTestId("sidebar:left")).contains(label).click();
+	cy.get(dataTestId("sidebar:left")).click();
 });
 
-Then(`the dropdown closes`, locale => {
-	cy.get(dataTestId("dropdown")).should("not.be.visible");
+Then(`the dropdown closes`, function () {
+	cy.get(dataTestId("dropdown")).should("not.exist");
 });
