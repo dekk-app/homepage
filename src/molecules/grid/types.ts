174d855508704f2332f21e18ca72db7f5b5c6cfb
Gregor Adams <greg@dekk.app>
2021-07-27 21:16:34 +0200

    feat(grid): allow calc and var

--- src/molecules/grid/types.ts
+++ src/molecules/grid/types.ts
@@ -1,12 +1,17 @@
 import { BasePropsWithInnerRef } from "@/types/components";
 
 export type ColCount = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
-export type ColSpan = ColCount;
-// ToDo This should work but causes rendering issues.
-//  The calc function does not return the desired result
-// export type CSSVariable = `var(${string})`;
-// export type CalcFunction = `calc(${string})`;
-// export type ColSpan = ColCount | CalcFunction | CSSVariable;
+export type CSSVariable = `var(${string})`;
+/** CalcFunction works but causes rendering issues in some cases.
+ *  The repeat function requires a positive <integer> while the calc function
+ *  could return a <number>
+ *  Divisions will never work, whilemultiplications, subtractions and additions
+ *  will not work if one side is a <number>
+ *  @link {@see https://drafts.csswg.org/css-values-3/#calc-notation}
+ *  @link {@see https://drafts.csswg.org/css-values-3/#calc-type-checking}
+ */
+export type CalcFunction = `calc(${string})`;
+export type ColSpan = ColCount | CalcFunction | CSSVariable;
 export type ColStart = ColCount;
 
 export interface StyledColumnProps {
