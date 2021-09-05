dd239f07a1f778843600e11f0c365d6bee896654
Gregor Adams <pixelass@users.noreply.github.com>
2021-09-05 09:23:36 +0200

    chore: enable all tests (#87)

--- src/ions/services/apollo/client.ts
+++ src/ions/services/apollo/client.ts
@@ -43,9 +43,8 @@ function createApolloClient() {
 		cache: new InMemoryCache().restore({}),
 		defaultOptions: {
 			query: {
-				fetchPolicy: "cache-first",
-				// Enable if you want to develop with uncached data
-				// fetchPolicy: process.env.NODE_ENV === "production" ? "cache-first" : "no-cache",
+				fetchPolicy:
+					process.env.NEXT_PUBLIC_NO_CACHE === "true" ? "no-cache" : "cache-first",
 				errorPolicy: "all",
 			},
 		},
