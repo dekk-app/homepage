e4d6798d0e3d761faeb13ed72c95c45bb82cefd5
Gregor Adams <greg@pixelass.com>
2021-09-23 18:43:07 +0200

    feat: add accordion to cards

--- src/molecules/accordion/types.ts
+++ src/molecules/accordion/types.ts
@@ -2,6 +2,7 @@ export interface AccordionProps {
 	heading: string;
 	id: string;
 	initiallyExpanded?: boolean;
+	ellipsis?: boolean;
 	faq?: boolean;
 	headerComponent?: "h2" | "h3" | "h4" | "h5" | "h6";
 }
